a056504746e33fab7cd613fb8b572bc91bec4967
Peushi Ariyawansa <peushi.ariyawansa@epita.fr>
2026-03-06 11:33:50 +0100

    Restructuring my code into folders -  coded type.ts

---
+++ src/domain/survivor/types.ts
@@ -0,0 +1,5 @@
+export type SurvivorId = string & { readonly __brand: "SurvivorId" }
+export type Health = number & { readonly __brand: "Health" }
+export type FoodStock = number & { readonly __brand: "FoodStock" }
+
+export type SurvivorName = "Lisa" | "Meli" | "Sana" | "Liz" | "Jess" | "Luc" | "Brad"
